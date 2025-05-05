6f7a6faf56123eab8bd7a93fda5104caff165c17
Mitch Fossen <git@mail.mitch.sh>
2025-05-04 21:14:48 -0500

    add urfave cli

--- cmd/emailer/emailer.go
+++ cmd/emailer/emailer.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
+
 func main() {
+	cmd := &cli.Command{
+		Commands: []*cli.Command{
+			&cli.Command{
+				Name:    "mailbox",
+				Aliases: []string{"mbox"},
+				Usage:   "Operations on mailboxes",
+				Commands: []*cli.Command{
+					&cli.Command{
+						Name:  "list",
+						Usage: "List mailboxes",
+						Action: func(c context.Context, cmd *cli.Command) error {
+							fmt.Println("Listing mailboxes")
+							return nil
+						},
+					},
+				},
+			},
+		},
+	}
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mfossen/emailer
 
 go 1.24.2
+
+require github.com/urfave/cli/v3 v3.3.2 // indirect
